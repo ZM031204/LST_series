2c24995c221bb5d6523106122de9bede7ba60982
ChanningTong <channing_tongcn@outlook.com>
2025-02-13 14:59:46 +0800

    [Bug fix] fix image download fault-- the last iamge would be created after the task was marked as finished

--- workflow_image.py
+++ workflow_image.py
@@ -6,7 +6,7 @@ from fetch_drive import get_folder_id_by_name as get_folder_id
 from dotenv import load_dotenv
 import os
 import ee
-
+import time
 def __main__():
     load_dotenv()
     SAVE_PATH = os.getenv('SERIES_SAVE_PATH')
@@ -24,6 +24,7 @@ def __main__():
     task = create_lst_image(year,month_list,SERIES_FOLDER_ID)
     is_success = check_task_status(task)
     if is_success:
+        time.sleep(30) # wait for the last iamge to be created
         folder_id = get_folder_id(drive,folder_name)
         download_and_clean(drive,folder_id, SAVE_PATH)
 
